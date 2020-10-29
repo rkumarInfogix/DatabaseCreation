e3c5a72fc11f5c7faaca138f0b80dc2b257c5754
Rajeev Kumar <rkumar@infogix.com>
2020-10-29 15:15:40 +0530

    Document updated

--- Redshift/CreateDatabaseAndUser.sql
+++ Redshift/CreateDatabaseAndUser.sql
@@ -1,6 +1,2 @@
 CREATE DATABASE TestCatalog;
-CREATE LOGIN [harvestertestuser] WITH PASSWORD='harvestertestuser', DEFAULT_DATABASE=[TestCatalog];
-CREATE USER [harvestertestuser] FOR LOGIN [harvestertestuser];
-ALTER USER [harvestertestuser] with DEFAULT_SCHEMA=TestSchema;
-grant select, execute, ALTER,insert,update,delete ON database::[TestCatalog] TO [harvestertestuser];
-
+create user harvestertestuser password 'Harvestertestuser1';
--- Redshift/DropDatabaseAndUser.sql
+++ Redshift/DropDatabaseAndUser.sql
@@ -1,5 +1,2 @@
-revoke select, execute, ALTER,insert,update,delete ON database::[TestCatalog] from  dberman;
-DROP login dberman;
-DROP user dberman;
-use master;
+drop user harvestertestuser;
 DROP DATABASE TESTCATALOG;
